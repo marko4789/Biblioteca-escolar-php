6f3bbe23e7f128f16caa32ececd4ecba2502f02a
marko4789 <markos4789@gmail.com>
2021-05-24 15:41:22 -0600

    Base de datos

---
+++ Base de Datos/bd_biblioteca.sql
@@ -0,0 +1,148 @@
+/*
+SQLyog Ultimate v11.11 (64 bit)
+MySQL - 5.5.5-10.4.14-MariaDB : Database - bd_biblioteca
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`bd_biblioteca` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+
+USE `bd_biblioteca`;
+
+/*Table structure for table `usuarios` */
+
+CREATE TABLE `usuarios` (
+  `idUsuario` int(11) NOT NULL AUTO_INCREMENT,
+  `nombreUsuario` varchar(30) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `apellidoPaterno` varchar(30) NOT NULL,
+  `apellidoMaterno` varchar(30) NOT NULL,
+  `password` varchar(32) NOT NULL,
+  `status` varchar(20) NOT NULL,
+  PRIMARY KEY (`idUsuario`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `usuarios` */
+
+/*Table structure for table `alumnos` */
+
+CREATE TABLE `alumnos` (
+  `idAlumno` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(50) NOT NULL,
+  `apellidoPaterno` varchar(30) NOT NULL,
+  `apellidoMaterno` varchar(30) DEFAULT NULL,
+  `matricula` varchar(9) NOT NULL,
+  `status` varchar(20) NOT NULL,
+  PRIMARY KEY (`idAlumno`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `alumnos` */
+
+/*Table structure for table `autores` */
+
+CREATE TABLE `autores` (
+  `idAutor` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(50) NOT NULL,
+  `apellidoPaterno` varchar(30) DEFAULT NULL,
+  `apellidoMaterno` varchar(30) DEFAULT NULL,
+  `status` varchar(20) NOT NULL,
+  PRIMARY KEY (`idAutor`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `autores` */
+
+/*Table structure for table `categorias` */
+
+CREATE TABLE `categorias` (
+  `idCategoria` int(11) NOT NULL AUTO_INCREMENT,
+  `categoria` varchar(60) NOT NULL,
+  `status` varchar(20) NOT NULL,
+  PRIMARY KEY (`idCategoria`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `categorias` */
+
+/*Table structure for table `editoriales` */
+
+CREATE TABLE `editoriales` (
+  `idEditorial` int(11) NOT NULL AUTO_INCREMENT,
+  `editorial` varchar(80) NOT NULL,
+  `status` varchar(20) NOT NULL,
+  PRIMARY KEY (`idEditorial`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `editoriales` */
+
+/*Table structure for table `libros` */
+
+CREATE TABLE `libros` (
+  `idLibro` int(11) NOT NULL AUTO_INCREMENT,
+  `titulo` varchar(200) NOT NULL,
+  `paginas` int(11) NOT NULL,
+  `pais` varchar(20) DEFAULT NULL,
+  `fechaPublicacion` date NOT NULL,
+  `idioma` varchar(20) DEFAULT NULL,
+  `isbn` varchar(13) DEFAULT NULL,
+  `existencia` int(11) NOT NULL,
+  `idCategoria` int(11) NOT NULL,
+  `idEditorial` int(11) NOT NULL,
+  `numlibro` varchar(5) NOT NULL,
+  `status` varchar(20) NOT NULL,
+  PRIMARY KEY (`idLibro`),
+  KEY `idCategoria` (`idCategoria`),
+  KEY `idEditorial` (`idEditorial`),
+  CONSTRAINT `libros_ibfk_1` FOREIGN KEY (`idCategoria`) REFERENCES `categorias` (`idCategoria`),
+  CONSTRAINT `libros_ibfk_2` FOREIGN KEY (`idEditorial`) REFERENCES `editoriales` (`idEditorial`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `libros` */
+
+/*Table structure for table `prestamos` */
+
+CREATE TABLE `prestamos` (
+  `idPrestamo` int(11) NOT NULL AUTO_INCREMENT,
+  `idLibro` int(11) NOT NULL,
+  `idUsuario` int(11) NOT NULL,
+  `idAlumno` int(11) NOT NULL,
+  `fechaPrestamo` date NOT NULL,
+  `fechaDevolucion` date DEFAULT NULL,
+  `status` varchar(20) NOT NULL,
+  PRIMARY KEY (`idPrestamo`),
+  KEY `idLibro` (`idLibro`),
+  KEY `idUsuario` (`idUsuario`),
+  KEY `idAlumno` (`idAlumno`),
+  CONSTRAINT `prestamos_ibfk_1` FOREIGN KEY (`idLibro`) REFERENCES `libros` (`idLibro`),
+  CONSTRAINT `prestamos_ibfk_2` FOREIGN KEY (`idUsuario`) REFERENCES `usuarios` (`idUsuario`),
+  CONSTRAINT `prestamos_ibfk_3` FOREIGN KEY (`idAlumno`) REFERENCES `alumnos` (`idAlumno`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `prestamos` */
+
+/*Table structure for table `relacion_autoria` */
+
+CREATE TABLE `relacion_autoria` (
+  `idAutoria` int(11) NOT NULL AUTO_INCREMENT,
+  `idAutor` int(11) NOT NULL,
+  `idLibro` int(11) NOT NULL,
+  PRIMARY KEY (`idAutoria`),
+  KEY `idAutor` (`idAutor`),
+  KEY `idLibro` (`idLibro`),
+  CONSTRAINT `relacion_autoria_ibfk_1` FOREIGN KEY (`idAutor`) REFERENCES `autores` (`idAutor`),
+  CONSTRAINT `relacion_autoria_ibfk_2` FOREIGN KEY (`idLibro`) REFERENCES `libros` (`idLibro`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `relacion_autoria` */
+
+
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
